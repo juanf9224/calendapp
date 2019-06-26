b8cd9b7ac1e1a6fb337d140d74e015a3a052b164
juanf9224 <juanfh.24.6@gmail.com>
2019-06-25 22:31:50 -0400

    - add weather service

--- src/app/provider/weather/weather.service.ts
+++ src/app/provider/weather/weather.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  resourceUrl = 'api.openweathermap.org/data/2.5/weather';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWeatherForeCast(city: string): Observable<HttpResponse<any>> {
+    return this.http.get(`${this.resourceUrl}/q=${city}`, {observe: 'response'});
+  }
 }
