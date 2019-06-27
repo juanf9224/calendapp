a8443c82b49b850185566d43dcd898ae3278f644
juanf9224 <juanfh.24.6@gmail.com>
2019-06-27 01:06:49 -0400

    - add cities dataset as constant for city name filter - update weather service with correct api and logic optimization - add utility functions to reminder dialog - add filter observable to use with cities field - add description field to reminder dialog - add tooltip to reminders icon with its description

--- src/app/calendar/reminder-dialog.component.ts
+++ src/app/calendar/reminder-dialog.component.ts
@@ -1,6 +1,6 @@
-import {Component, EventEmitter, Inject, OnInit, Output} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef, MatSnackBar} from '@angular/material';
-import {IReminder} from '../shared/model/reminder.model';
+import {IReminder, Reminder} from '../shared/model/reminder.model';
 import * as moment from 'moment';
 import {DAY_TIME, BULK_CITIES} from '../shared/constants';
 import {ReminderService} from '../shared/service/reminder.service';
@@ -15,10 +15,10 @@ import { ICity } from '../shared/model/city.model';
 })
 export class ReminderDialogComponent implements OnInit {
   dayTime = DAY_TIME;
-  cities: ICity[] = BULK_CITIES;
+  cities: ICity[] = [...BULK_CITIES];
   currentDate = new Date();
-  forecast: string;
-  filteredOptions: Observable<ICity[]>;
+  forecast: any;
+  filteredOptions: Observable<string[]>;
 
   constructor(
     public dialogRef: MatDialogRef<ReminderDialogComponent>,
@@ -30,28 +30,70 @@ export class ReminderDialogComponent implements OnInit {
 
   ngOnInit(): void {
     if (this.data && this.data.city) {
-      this.weatherService.getWeatherForeCast(this.data.city)
-        .subscribe(w => this.forecast = w.body.weather.main);
+      this.weatherService.fetchWeatherForeCast(this.data.city)
+        .subscribe(w => this.lookupForecast(w.body.list, this.data.date || moment()));
+    } else {
+      this.data = new Reminder();
     }
-
-    this.filteredOptions = of(this.cities).pipe(
-      startWith(''),
-      map((value: ICity) => this._filter(value.city))
-    );
-
   }
 
-  private _filter(value: string): ICity[] {
+  private lookupForecast(list: any[], date: moment.Moment) {
+    this.forecast = list.find(f => this.isSameDate(moment(f.dt_txt), date)).weather[0];
+    console.log(this.forecast);
+  }
+
+  onCityChanges(): void {
+    console.log('city change');
+    if (this.data && this.data.city && this.data.city.length >= 4) {
+      this.filteredOptions = of(this.data.city).pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+    }
+  }
+
+  private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
-    return BULK_CITIES.filter(option => option.city.toLowerCase().includes(filterValue));
+    return this.cities
+      .filter(option => option.city.toLowerCase().includes(filterValue))
+      .map(c => `${c.city}, ${c.country}`);
   }
 
   onAddClick(reminder: IReminder): void {
-    if (reminder && reminder.date && moment(reminder.date).isSameOrAfter(moment(new Date()))) {
-      this.reminderService.reminder = reminder;
-      this.dialogRef.close();
-    } else {
-      this.snackBar.open('Could not create reminder', 'Ok', {duration: 4000});
+    if (reminder && reminder.date) {
+      const hours = parseInt(`${reminder.time}`.substring(0, reminder.time.indexOf(':')), 10);
+      const minutes = parseInt(`${reminder.time}`.substring(reminder.time.indexOf(':'), reminder.time.length), 10);
+      // reminder.date = moment(`${reminder.date}`).add(hours, 'hours').add(minutes, 'minutes');
+      const createdReminder = this.createReminder(reminder, hours, minutes);
+      console.log(createdReminder.date);
+      if (moment(createdReminder.date).isSameOrAfter(moment())) {
+        this.reminderService.reminder = createdReminder;
+        this.dialogRef.close();
+      } else {
+        this.handleError(`
+          Could not create reminder, date should not be a past date time:
+          actual: ${moment()} reminder: ${createdReminder.date}`);
+      }
     }
+  }
+
+  private createReminder(rem: IReminder, hours: number, minutes: number): IReminder {
+    const reminder = Object.assign(new Reminder(), rem);
+    reminder.date = moment(rem.date).hours(hours).minutes(minutes);
+    return reminder;
+  }
+
+  clearReminder(): void {
+    this.reminderService.reminder = null;
+  }
+
+  private handleError(msg: string, action?: string, waitTime?: number) {
+    this.snackBar
+      .open(msg,
+        action || 'Ok', {duration: waitTime || 4000});
+  }
+
+  private isSameDate(a: moment.Moment, b: moment.Moment): boolean {
+      return a.month() === b.month() && a.date() === b.date();
   }
 }
